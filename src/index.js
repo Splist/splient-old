6d734b8fa36eb7fb22973e377a9dd71e30cfc09e
Grey <chronomly@gmail.com>
2019-08-13 09:15:49 -0400

    chore: add semicolons beginnings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, ipcMain} = require('electron')
-const path = require('path')
+const {app, BrowserWindow, ipcMain} = require('electron');
+const path = require('path');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
@@ -15,7 +15,7 @@ function createWindow () {
           preload: path.join(__dirname, 'preload.js')
         },
         autoHideMenuBar: true
-    })
+    });
 
     app.on('browser-window-created',function(e,window) {
         mainWindow.setMenu(null);
@@ -26,11 +26,11 @@ function createWindow () {
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      mainWindow = null;
+    });
 }
 
 // This method will be called when Electron has finished
@@ -42,18 +42,11 @@ ipcMain.on('pageLoaded', () => {
     console.log('Hello World!')
 })
 
-// Quit when all windows are closed.
+// macOS bad
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  if (mainWindow === null) createWindow();
+});
--- src/preload.js
+++
@@ -1,7 +0,0 @@
-import { ipcMain } from "electron";
-
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  ipcMain.emit('pageLoaded');
-})
